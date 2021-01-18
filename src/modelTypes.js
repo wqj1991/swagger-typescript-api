02b44a539ef6afdc7a65f73a6e491e1ce59e6aeb
王庆军 <w21976169@163.com>
2021-01-18 11:00:10 +0800

    use any replace object

--- src/modelTypes.js
+++ src/modelTypes.js
@@ -2,6 +2,7 @@ const { formatters } = require("./typeFormatters");
 const { checkAndRenameModelName } = require("./modelNames");
 const { config } = require("./config");
 const { getTypeData } = require("./components");
+const _ = require("lodash");
 
 const prepareModelType = (typeInfo) => {
   const typeData = getTypeData(typeInfo);
@@ -9,6 +10,18 @@ const prepareModelType = (typeInfo) => {
 
   if (config.generateUnionEnums && typeIdentifier === "enum") {
     typeIdentifier = "type";
+  }
+
+  if (typeIdentifier === "interface" && content && content.length > 0) {
+    _.forEach(content, (e) => {
+      if (e.field) {
+        let field = e.field;
+        if (field) {
+          const arr = _.split(field, ":");
+          if (arr.length === 2) e.field = arr[0] + ":" + _.replace(arr[1], "object", "any");
+        }
+      }
+    });
   }
 
   const resultContent = formatters[type] ? formatters[type](content) : content;
